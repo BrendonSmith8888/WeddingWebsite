0578ea9f7c21d79de14d8ad8b0b81801c29b9172
Brendon Smith <brendonsmith8888@gmail.com>
2022-01-17 19:30:22 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
 import './App.css';
+import picture from './Test.jpg';
+import React from 'react';
 
-function App() {
+/* function App() {
   return (
-    <div className='weddingMain'>
-      <p>Insert Top Navigation links</p>
-      <p>Image</p>
-      <h1>Tracey and Jamie's Wedding</h1>
-      <p>How We Met</p>
-      <p>Venue</p>
-      <p>RSPV</p>
-      <p>Copyright</p>
+    <div className='weddingMain' style={{ backgroundColor: '#5f9ea0'}}>
+      <header>
+        <p>Insert Top Navigation links</p>
+      </header>
+      <body>
+        <img alt='test' src={picture} style={{height: 800, width: 800}} />
+        <h1>Tracey and Jamie's Wedding</h1>
+        <p>How We Met</p>
+        <p>Schedule</p>
+        <p>Wedding</p>
+        <p>Travel Info</p>
+        <p>Copyright</p>
+      </body> 
     </div>
   );
+} */
+
+class App extends React.Component {
+  render() {
+  return (
+    <div style={{ backgroundColor: '#5f9ea0'}}>
+      <header>
+        <p>Insert Top Navigation links</p>
+      </header>
+      <body>
+        <img alt='test' src={picture} style={{ height: 800, width: 800}} />
+        <h1>Tracey and Jamie's Wedding</h1>
+        <p>How We Met</p>
+        <p>Schedule</p>
+        <p>Wedding</p>
+        <p>Travel Info</p>
+        <p>Copyright</p>
+      </body> 
+    </div>
+  );
+}
 }
 
 export default App;
