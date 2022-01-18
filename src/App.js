0a9494be3060cc7c88d712d825c192253496bb54
Brendon Smith <brendonsmith8888@gmail.com>
2022-01-18 12:11:11 +0200

    2nd Update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import picture from './Test.jpg';
-import React from 'react';
+import picture from "./Test.jpg";
+import React, { Component } from "react";
 
 /* function App() {
   return (
@@ -21,25 +21,29 @@ import React from 'react';
   );
 } */
 
-class App extends React.Component {
+class App extends Component {
   render() {
-  return (
-    <div style={{ backgroundColor: '#5f9ea0'}}>
-      <header>
-        <p>Insert Top Navigation links</p>
-      </header>
-      <body>
-        <img alt='test' src={picture} style={{ height: 800, width: 800}} />
-        <h1>Tracey and Jamie's Wedding</h1>
-        <p>How We Met</p>
-        <p>Schedule</p>
-        <p>Wedding</p>
-        <p>Travel Info</p>
-        <p>Copyright</p>
-      </body> 
-    </div>
-  );
-}
+    return (
+      <div>
+        <header>
+          <ul>How We Met</ul>
+          <ul>Schedule</ul>
+          <ul>Wedding Info</ul>
+          <ul>Travel Info</ul>
+          <ul>RSPV</ul>
+        </header>
+        <body> 
+          <img alt="test" src={picture} style={{ height: 800, width: 800 }} />
+          <h1>Tracey and Jamie's Wedding</h1>    
+          <p>How We Met</p>
+          <p>Schedule</p>
+          <p>Wedding</p>
+          <p>Travel Info</p>
+          <p>RSPV</p>
+        </body>
+      </div>
+    );
+  }
 }
 
 export default App;
