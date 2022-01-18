5dfabdf538c1767d48fed45052df04f03ef220e6
Brendon Smith <brendonsmith8888@gmail.com>
2022-01-18 13:57:32 +0200

    3rd Update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
-import picture from "./Test.jpg";
-import React, { Component } from "react";
+import picture from './Test.jpg';
+import React, { Component } from 'react';
+// import {Image} from 'react-native';
 
 /* function App() {
   return (
@@ -24,22 +25,27 @@ import React, { Component } from "react";
 class App extends Component {
   render() {
     return (
-      <div>
-        <header>
-          <ul>How We Met</ul>
-          <ul>Schedule</ul>
-          <ul>Wedding Info</ul>
-          <ul>Travel Info</ul>
-          <ul>RSPV</ul>
+      <div className='shadow-1'>
+           <header className='shadow-1'>
+          <ul className='grow'>How We Met</ul>
+          <ul className='grow'>Schedule</ul>
+          <ul className='grow'>Wedding Info</ul>
+          <ul className='grow'>Travel Info</ul>
+          <ul className='grow'>RSPV</ul>
         </header>
-        <body> 
-          <img alt="test" src={picture} style={{ height: 800, width: 800 }} />
-          <h1>Tracey and Jamie's Wedding</h1>    
-          <p>How We Met</p>
-          <p>Schedule</p>
-          <p>Wedding</p>
-          <p>Travel Info</p>
-          <p>RSPV</p>
+        <body className='shadow-1'>  
+          <img alt="test" src={picture} />
+          <h1>Welcome to Tracey and Jamie's Wedding</h1>    
+          <h2>How We Met</h2>
+          <p>Cayman Islands</p>
+          <h2>Schedule</h2>
+          <p>TBC</p>
+          <h2>Wedding</h2>
+          <p>TBC</p>
+          <h2>Travel Info</h2>
+          <p>Somewhere in South Africa</p>
+          <h2>RSPV</h2>
+          <p>Reply Yes or No</p>
         </body>
       </div>
     );
